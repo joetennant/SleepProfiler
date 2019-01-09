9c946a6e0d853339f8cff1b2e7c824081c6d87c1
Joe Tennant <joetennant@gmail.com>
2019-01-09 10:07:28 -0500

    Initial Commit

---
+++ SleepProfiler/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SleepProfiler
+//
+//  Created by Joseph Tennant on 1/9/19.
+//  Copyright © 2019 7SIGNAL. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
